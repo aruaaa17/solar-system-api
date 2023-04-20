f06d39e01a52a1c84d226523ed6333e45247b030
April Zhang <aprilzh23@gmail.com>
2023-04-20 16:24:27 -0700

    Created Planets Routes

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,11 @@ from flask import Flask
 def create_app(test_config=None):
     app = Flask(__name__)
 
+    from flask import Blueprint
+    from .routes import planets_bp
+    
+    app.register_blueprint(planets_bp)
+
     return app
 
 
--- app/routes.py
+++ app/routes.py
@@ -1,27 +1,49 @@
 from flask import Blueprint, jsonify
 
+class Planet():
+    def __init__(self, id, name, description, distance):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.distance = distance
 
 
+planet_list = [
+    Planet(1, "Mercury", "A rocky planet", 1),
+    Planet(2, "Venus", "A rocky planet", 2),
+    Planet(3, "Earth", "A rocky planet", 3),
+    Planet(4, "Mars", "A gas planet", 4),
+    Planet(5, "Jupiter", "A gas planet", 5),
+    Planet(6, "Saturn", "A gas planet", 6),
+    Planet(7, "Uranus", "A ice planet", 7),
+    Planet(8, "Neptune", "A ice planet", 8)
+]
 
 
-planets_bp = Blueprint("",)
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
-# class Animal():
+@planets_bp.route("", methods=['GET'])
+def handle_planets():
+    planet_list_as_dict = [vars(planet) for planet in planet_list]
+    return jsonify(planet_list_as_dict), 200
+
+
+# class planet():
 #     def __init__(self, id, species, name, habitat):
 #         self.id = id
 #         self.species = species
 #         self.name = name
 #         self.habitat = habitat
 
-# sapphire_animals = [
-#     Animal(1, "Anaconda", "Nikki Minaj", "Jungle"),
-#     Animal(2, "Elephant", "Dumbo", "Our childhood!!!"),
-#     Animal(3, "Unicorn", "Charlie", "Youtube")
+# planet_list = [
+#     planet(1, "Anaconda", "Nikki Minaj", "Jungle"),
+#     planet(2, "Elephant", "Dumbo", "Our childhood!!!"),
+#     planet(3, "Unicorn", "Charlie", "Youtube")
 # ]
 
-# animals_bp = Blueprint("animals", __name__, url_prefix="/animals")
+# planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
-# @animals_bp.route("", methods=['GET'])
-# def handle_animals():
-#     sapphire_animals_as_dict = [vars(animal) for animal in sapphire_animals]
-#     return jsonify(sapphire_animals_as_dict), 200
+# @planets_bp.route("", methods=['GET'])
+# def handle_planets():
+#     planet_list_as_dict = [vars(planet) for planet in planet_list]
+#     return jsonify(planet_list_as_dict), 200
