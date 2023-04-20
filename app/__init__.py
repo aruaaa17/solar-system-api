9dc89f935e22b3e1c02cd100d1d7a8cf13d6739f
Edith <mxedithbaltazar@gmail.com>
2023-04-20 11:39:51 -0700

    notes

--- app/__init__.py
+++ app/__init__.py
@@ -5,3 +5,17 @@ def create_app(test_config=None):
     app = Flask(__name__)
 
     return app
+
+
+# from flask import Flask
+
+# def create_app():
+#     app = Flask(__name__)
+
+#     #  add our new animals blueprint
+#     from flask import Blueprint
+#     from .routes.animals import animals_bp
+
+#     app.register_blueprint(animals_bp)
+
+#     return app 
